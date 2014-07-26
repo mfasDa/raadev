b6ee9334a07d33e20e8a31d4a71d30f862a9ad54
mfasel <mfasel@lbl.gov>
2014-07-26 19:28:45 +0200

    Prepare add macro for the first grid run

--- macros/AddTaskPtEMCalTrigger.C
+++ macros/AddTaskPtEMCalTrigger.C
@@ -1,4 +1,26 @@
-void AddTaskPtEMCalTrigger(){
+AliAnalysisTask* AddTaskPtEMCalTrigger(){
+        AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+         
+        if (!mgr) {
+             ::Error("AddTaskPtEMCalTrigger", "No analysis manager to connect to.");
+             return NULL;
+        }
+         
+        if (!mgr->GetInputEventHandler()) {
+             ::Error("AddTaskPtEMCalTrigger", "This task requires an input event handler");
+             return NULL;
+        }
+        
         AliAnalysisTaskPtEMCalTrigger *pttriggertask = new AliAnalysisTaskPtEMCalTrigger("ptemcaltriggertask");
         pttriggertask->SelectCollisionCandidates(AliVEvent::kINT7 | AliVEvent::kEMC7);                          // Select both INT7 or EMC7 triggered events
+        mgr->AddTask(pttriggertask);
+
+        AliAnalysisDataContainer *cinput = mgr->GetCommonInputContainer();
+        AliAnalysisDataContainer *coutput = mgr->CreateContainer("results", TList::Class(),    AliAnalysisManager::kOutputContainer, "AnalysisResults.root");
+   
+        //Connect input/output
+        mgr->ConnectInput(task, 0, cinput);
+        mgr->ConnectOutput(task, 1, coutput);
+           
+        return task;
 }
