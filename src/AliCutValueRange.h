2b209e31890acee1c1ca65f4d95f65d7ab79ddff
mfasel <mfasel@lbl.gov>
2014-07-29 16:17:10 +0200

    Add documentation so that the class can go to aliroot

--- src/AliCutValueRange.h
+++ src/AliCutValueRange.h
@@ -1,5 +1,9 @@
 #ifndef ALICUTVALUERANGE_H
 #define ALICUTVALUERANGE_H
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+// Author: Markus Fasel
 
 #include <TObject.h>
 
