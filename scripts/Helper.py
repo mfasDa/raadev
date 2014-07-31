be68d5cccd5c635be078b032cd24878829ac791b
mfasel <mfasel@lbl.gov>
2014-07-31 14:47:30 +0200

    Move exception to Helper module and fix the string conversion in both exceptions

--- scripts/Helper.py
+++ scripts/Helper.py
@@ -2,6 +2,46 @@
 #
 # Helper tools for spectrum plotting
 #
+#    Author: Markus Fasel
+#
+
+class FileReaderException(Exception):
+        """
+        Exception class handling root files which are
+        either not found or not readable.
+        """
+
+        def __init__(self, filename):
+                """
+                Constructor, assigning the name of the file which 
+                failed to be read.
+                """
+                self.__filename == filename
+
+        def __str__(self):
+                """
+                Create string representation of the error message
+                """
+                return "Could not open file %s" %(self.__filename)
+
+class HistNotFoundException(Exception):
+        """
+        Exception class handles the case that a histogram
+        is not found in the file / list
+        """
+
+        def __init__(self, histname):
+                """
+                Constructor, assigning the name of the histogram
+                which was not found.
+                """
+                self.__histname = histname
+
+        def __str__(self):
+                """
+                Create string representation of the error message
+                """
+                return "Histogram %s not found" %(self.__histname)
 
 def NormaliseBinWidth(hist):
         """
