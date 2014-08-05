51620e770729f8f4f4dc939104193c240cf829cf
mfasel <mfasel@lbl.gov>
2014-08-05 16:20:10 +0200

    Add hybrid track cuts as second cut configuration

--- macros/AddTaskPtEMCalTrigger.C
+++ macros/AddTaskPtEMCalTrigger.C
@@ -1,3 +1,10 @@
+#if !defined (__CINT__) || defined (__MAKECINT__)
+#include "AliAnalysisManager.h"
+#include "AliAnalysisTaskPtEMCalTrigger.h"
+#include "AliESDtrackCuts.h"
+#include <TList.h>
+#endif
+
 AliAnalysisTask* AddTaskPtEMCalTrigger(){
         AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
          
@@ -13,11 +20,24 @@ AliAnalysisTask* AddTaskPtEMCalTrigger(){
         
         EMCalTriggerPtAnalysis::AliAnalysisTaskPtEMCalTrigger *pttriggertask = new EMCalTriggerPtAnalysis::AliAnalysisTaskPtEMCalTrigger("ptemcaltriggertask");
         pttriggertask->SelectCollisionCandidates(AliVEvent::kINT7 | AliVEvent::kEMC7);                          // Select both INT7 or EMC7 triggered events
-        AliESDtrackCuts *trackCuts = AliESDtrackCuts::GetStandardITSTPCTrackCuts2011(true, 1);
-        trackCuts->SetMinNCrossedRowsTPC(120);
-        trackCuts->SetMaxDCAToVertexXYPtDep("0.0182+0.0350/pt^1.01");
-        pttriggertask->SetTrackCuts(trackCuts);
         mgr->AddTask(pttriggertask);
+
+        // Create charged hadrons pPb standard track cuts
+        AliESDtrackCuts *standardTrackCuts = AliESDtrackCuts::GetStandardITSTPCTrackCuts2011(true, 1);
+        standardTrackCuts->SetName("Standard Track cuts");
+        standardTrackCuts->SetMinNCrossedRowsTPC(120);
+        standardTrackCuts->SetMaxDCAToVertexXYPtDep("0.0182+0.0350/pt^1.01");
+        pttriggertask->AddTrackCuts(standardTrackCuts);
+
+        // Create hybrid track cuts as used in the jet analysis
+        AliESDtrackCuts* hybridTrackCuts = AliESDtrackCuts::GetStandardITSTPCTrackCuts2011(kFALSE);
+        hybridTrackCuts->SetName("Global Hybrid tracks, loose DCA");
+        hybridTrackCuts->SetMaxDCAToVertexXY(2.4);
+        hybridTrackCuts->SetMaxDCAToVertexZ(3.2);
+        hybridTrackCuts->SetDCAToVertex2D(kTRUE);
+        hybridTrackCuts->SetMaxChi2TPCConstrainedGlobal(36);
+        hybridTrackCuts->SetMaxFractionSharedTPCClusters(0.4);
+        pttriggertask->AddTrackCuts(hybridTrackCuts);
 
         AliAnalysisDataContainer *cinput = mgr->GetCommonInputContainer();
         AliAnalysisDataContainer *coutput = mgr->CreateContainer("results", TList::Class(),    AliAnalysisManager::kOutputContainer, "AnalysisResults.root");
