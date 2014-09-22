e102d66f1898fea18ae8844290bb39fb2bdb2cc4
mfasel <mfasel@lbl.gov>
2014-09-22 13:36:05 +0200

    Restructure code so that it can handle different parameterisations. - Add default parameterisations power law and modified hagedorn - Restrict modified hagedorn so that the fit will converge

--- analysis/base/SpectrumFitter.py
+++ analysis/base/SpectrumFitter.py
@@ -6,6 +6,46 @@ Created on 17.09.2014
 
 from ROOT import TF1, TGraph, TH1F
 
+class FitModel:
+    
+    def __init__(self):
+        self._model = None
+        
+    def GetFunction(self):
+        return self._model
+    
+    def SetFunctionName(self, name):
+        self._model.SetName(name)
+        
+    def GetFunctionName(self):
+        return self._model.GetName()
+    
+class PowerLawModel(FitModel):
+    
+    def __init__(self):
+        self._model = TF1("fitfunctionPowerlaw", "[0] * TMath::Power(x,[1])", 0., 100.)
+    
+class ModifiedHagedornModel(FitModel):
+    
+    def __init__(self):
+        self._model = TF1("fitfunctionModHagedorn", "[0]/TMath::Power(TMath::Exp(-[1]*x - [2]*x*x) + x/[3], [4])", 0., 100.)
+        self._model.SetParName(0, "A")
+        self._model.SetParName(1, "a")
+        self._model.SetParName(2, "b")
+        self._model.SetParName(3, "p0")
+        self._model.SetParName(4, "n")
+        # Force all parameters to be positive 
+        self._model.SetParLimits(0, 1e-5, 10000)
+        self._model.SetParLimits(1, 1e-3, 1)
+        self._model.SetParLimits(2, 1e-3, 1)
+        self._model.SetParLimits(3, 1e-3, 1)
+        self._model.SetParLimits(4, 1e-5, 10)
+        self._model.SetParameter(0, 100)
+        self._model.SetParameter(1, 0.5)
+        self._model.SetParameter(2, 0.5)
+        self._model.SetParameter(3, 0.5)
+        self._model.SetParameter(4, 5.)
+
 class SpectrumFitter:
     '''
     Class fitting a raw spectrum
@@ -19,43 +59,38 @@ class SpectrumFitter:
         def __str_(self):
             return "Fit of the spectrum not yet performed"
 
-    def __init__(self, name, spectrum):
+    def __init__(self, name, spectrum, fitmodel = None):
         '''
         Constructor
         '''
         self.__name = name
         self.__data = spectrum
-        self.__model = TF1("fitfunction%s"%(self.__name), "[0] * TMath::Power(x,[1])", 0., 100.)
+        self.__model = fitmodel
+        if not self.__model:
+            self.__model = PowerLawModel()
         self.__fitDone = False
         
+    def SetFitModel(self, fitmodel):
+        self.__model = fitmodel
+        
     def DoFit(self, rangemin, rangemax = 50):
-        self.__data.Fit("fitfunction%s"%(self.__name), "N", "", rangemin, rangemax)
+        self.__data.Fit(self.__model.GetFunction(), "N", "", rangemin, rangemax)
         self.__fitDone = True
         
     def GetParameterisation(self):
         if not self.__fitDone:
             raise self.SpectrumFitterException()
-        return self.__model
+        return self.__model.GetFunction()
     
     def GetParameterisedValueAt(self, x):
         if not self.__fitDone:
             raise self.SpectrumFitterException()
-        return self.__model.Eval(x)
-    
-    def GetIntegralAbove(self, x):
-        if not self.__fitDone:
-            raise self.SpectrumFitterException()
-        return self.__antider.Eval(x)
+        return self.__model.GetFunction().Eval(x)
    
-    def GetNormalisedIntegralAbove(self, x):
-        if not self.__fitDone:
-            raise self.SpectrumFitterException()
-        return self.__antiderNorm.Eval(x)
-
     def CalculateIntegralAbove(self, x):
         if not self.__fitDone:
             raise self.SpectrumFitterException()
-        return self.__model.Integral(x, 10000)
+        return self.__model.GetFunction().Integral(x, 10000)
    
     def CalculateNormalisedIntegralAbove(self, x):
         """
@@ -68,7 +103,7 @@ class SpectrumFitter:
         ptstart = x
         yieldval = 0
         while ptstart < maxint:
-            yieldval += self.__model.Integral(ptstart, ptstart+10)/10.
+            yieldval += self.__model.GetFunction().Integral(ptstart, ptstart+10)/10.
             ptstart += 10
         return yieldval
             
@@ -91,25 +126,25 @@ class SpectrumFitter:
     def CalculateIntegral(self, xmin, xmax):
         if not self.__fitDone:
             raise self.SpectrumFitterException()
-        return self.__model.Integral(xmin, xmax)
+        return self.__model.GetFunction().Integral(xmin, xmax)
     
     def CalculateBinMean(self, xmin, xmax):
         if not self.__fitDone:
             raise self.SpectrumFitterException()
-        return self.__model.Integral(xmin, xmax)/(xmax - xmin)
+        return self.__model.GetFunction().Integral(xmin, xmax)/(xmax - xmin)
                 
     def GetFitFunction(self):
-        return self.__model
+        return self.__model.GetFunction()
         
 class MinBiasFitter(SpectrumFitter):
     
-    def __init__(self, name, data, fitmin = 15.):
+    def __init__(self, name, data, fitmin = 15., model = None):
         SpectrumFitter.__init__(self, name, data)
         self.DoFit(fitmin, 50.)
         
 class TriggeredSpectrumFitter(SpectrumFitter):
     
-    def __init__(self, name, data):
+    def __init__(self, name, data, model = None):
         SpectrumFitter.__init__(self, name, data)
         self.DoFit(50., 90.)
 
