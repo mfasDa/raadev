64133a72940cbe2c756cdeeac7bc1227d71c07a9
mfasel <mfasel@lbl.gov>
2014-10-01 11:25:01 +0200

    Do error handling when adding a new spectrum fails

--- analysis/base/SpectraSum.py
+++ analysis/base/SpectraSum.py
@@ -9,6 +9,23 @@ A spectrum can be every type that provides an add function (like ROOT THn's)
 
 from copy import deepcopy
 
+class SummingException(Exception):
+    """
+    Error handling for spectrum summing
+    """
+    
+    def __init__(self, message):
+        """
+        Constructor
+        """
+        self.__message = message
+    
+    def __str__(self):
+        """
+        Get string representation of the exception
+        """
+        return self.__message
+
 class SpectraSum(object):
     """
     Class summing up different spectra
@@ -24,10 +41,19 @@ class SpectraSum(object):
         """
         Add spectrum. If it is the first spectrum, initialise the sum with a deep copy of this. Otherwise add the the spectrum to the sum
         """
+        print "Add new spectrum"
         if not self.__summed:
-            self.__summed = deepcopy(spectrum)
+            try:
+                self.__summed = deepcopy(spectrum)
+            except:
+                raise SummingException("Copy of the initial object failed")
         else:
-            self.__summed.Add(spectrum)
+            try:
+                self.__summed.Add(spectrum)
+            except Exception as e:
+                raise SummingException("Exception occurred in adding spectrum: %s" %(str(e)))
+        if not self.__summed:
+            raise SummingException("Nullpointer to the summed object")
             
     def GetSummedSpectrum(self):
         """
