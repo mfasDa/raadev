651eacc1008b62a4b051e74388bde8f965ba4256
mfasel <markus.fasel@cern.ch>
2015-04-20 21:42:31 +0200

    More documentation

--- analysis/util/SpectrumTools.py
+++ analysis/util/SpectrumTools.py
@@ -1,9 +1,29 @@
+#**************************************************************************
+#* Copyright(c) 1998-2014, ALICE Experiment at CERN, All rights reserved. *
+#*                                                                        *
+#* Author: The ALICE Off-line Project.                                    *
+#* Contributors are mentioned in the code where appropriate.              *
+#*                                                                        *
+#* Permission to use, copy, modify and distribute this software and its   *
+#* documentation strictly for non-commercial purposes is hereby granted   *
+#* without fee, provided that the above copyright notice appears in all   *
+#* copies and that both the copyright notice and this permission notice   *
+#* appear in the supporting documentation. The authors make no claims     *
+#* about the suitability of this software for any purpose. It is          *
+#* provided "as is" without express or implied warranty.                  *
+#**************************************************************************
 """
 Toolset module for histogram operations
 
+Original author (ROOT macro):
 @author: Jacek Otwinowski
 @organization: ALICE Collaboration
-Translated into PYTHON by Markus Fasel <markus.fasel@cern.ch>, Lawrence Berkeley National Laboratory
+Translated into PYTHON by  
+@author: Markus Fasel
+@contact: markus.fasel@cern.ch
+@organization: ALICE Collaboration
+@organization: Lawrence Berkeley National Laboratory
+@copyright: 1998-2014, ALICE Experiment at CERN, All rights reserved.
 """
 
 from ROOT import TF1, TGraph, TGraphAsymmErrors, TGraphErrors, TMultiGraph
