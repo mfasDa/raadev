4412915f5d5cc40b933be67cce698fbef643f3ed
mfasel <markus.fasel@cern.ch>
2015-04-23 19:01:31 +0200

    Do several fixes in the mergescript

--- scripts/RemergeLast.py
+++ scripts/RemergeLast.py
@@ -12,37 +12,44 @@ def RecursiveMerge(outputfile, listoffiles, bucketfactor, sandbox, iteration = 0
     if not os.path.exists(sandbox):
         os.makedirs(sandbox, 0755)
     stagedir = "%s/iter%d" %(sandbox, iteration)
+    if not os.path.exists(stagedir):
+        os.mkdir(stagedir)
     rootfile = os.path.basename(outputfile)
     listfornext = []
     patchcounter = 0
     package = []
     for f in listoffiles:
-        package.append(f)
+        if os.path.exists(f):
+            print "Adding file %s" %f
+            package.append(f)
         if len(package) >= bucketfactor:
             mergedfile = "%s/%d/%s" %(stagedir, patchcounter, rootfile)
-            MergeBucket(mergedfile, listoffiles)
+            os.makedirs(os.path.dirname(mergedfile), 0755)
+            MergeBucket(mergedfile, package)
             patchcounter +=1
             package = []
             listfornext.append(mergedfile)
     # merge last bunch
     if len(package):
         mergedfile = "%s/%d/%s" %(stagedir, patchcounter, rootfile)
+        os.makedirs(os.path.dirname(mergedfile), 0755)
         MergeBucket(mergedfile, listoffiles)
         listfornext.append(mergedfile)
     if len(listfornext) == 1:
         # we are done
         # move output file to its final location
         # clean sandbox
-        shutil.move_file(listfornext[0], outputfile)    
+        shutil.move(listfornext[0], outputfile)    
         os.rmdir(sandbox)
     else:
         # next iteration step
         RecursiveMerge(outputfile, listfornext, bucketfactor, sandbox, iteration+1)
         
-def FindListStage(inputdirs):
+def FindLastStage(inputdirs):
     maxID = -1
     for f in inputdirs:
-        if not "Stage_" in inputdirs:
+        print f
+        if not "Stage_" in f:
             continue
         stageID = int(f.replace("Stage_", ""))
         if stageID > maxID:
@@ -53,26 +60,28 @@ def FindListStage(inputdirs):
 
 def FindRecursive(inputpath, rootfilename):
     result = []
-    dirpath, dirlist, files = os.walk(inputdir)
-    for f in files:
-        if f == rootfilename:
-            result.append("%s/%s" %(inputpath,f))
-    for d in dirlist:
-        tmplist = FindRecursive("%s/%s" %(inputpath, d), rootfilename)
-        for f in tmplist:
-            result.append(f)
+    for dirpath, dirlist, files in os.walk(inputpath):
+        for f in files:
+            if f == rootfilename:
+                result.append("%s/%s" %(inputpath,f))
+        for d in dirlist:
+            tmplist = FindRecursive("%s/%s" %(inputpath, d), rootfilename)
+            for f in tmplist:
+                result.append(f)
     return result
 
 def RunMerge(inputdir, rootfile):
     for r in os.listdir(inputdir):
+        print "Doing run %s" %r
         for b in os.listdir("%s/%s" %(inputdir, r)):
+            print "Doing bin %s" %b
             stageToMerge = FindLastStage(os.listdir("%s/%s/%s" %(inputdir, r, b)))
-            inputdir = "%s/%s/%s/%s" %(inputdir, r, b, stageToMerge)
-            outputfile = "%s/%s/%s/%s" %(inputdir, r, b, rootfile)
             if stageToMerge:
                 # create filelist
                 print "merging run %s, bin %s" %(r, b)
-                RecursiveMerge(outputfile, FindRecursive(inputdir, rootfile), 10, "/tmp/%s/merge" %(getpass.getuser()))
+                mergeinputdir = "%s/%s/%s/%s" %(inputdir, r, b, stageToMerge)
+                outputfile = "%s/%s/%s/%s" %(inputdir, r, b, rootfile)
+                RecursiveMerge(outputfile, FindRecursive(mergeinputdir, rootfile), 10, "/tmp/%s/merge" %(getpass.getuser()))
                 
 if __name__ == "__main__":
     RunMerge(sys.argv[1], sys.argv[2])
