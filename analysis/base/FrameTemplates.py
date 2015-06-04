4935d05236181e2d7cc1a54d3638421e7c17a6a9
mfasel <markus.fasel@cern.ch>
2015-06-04 10:13:29 +0200

    Update sphinx documentation

--- analysis/base/FrameTemplates.py
+++ analysis/base/FrameTemplates.py
@@ -15,12 +15,12 @@
 """
 Helper module with pre-defined frames 
 
-@author: Markus Fasel , 
-@contact: <markus.fasel@cern.ch>
-@organization: Lawrence Berkeley National Laboratory
-@organization: ALICE Collaboration
-@copyright: 1998-2014, ALICE Experiment at CERN, All rights reserved
-@date: Apr 20, 2015
+:organization: ALICE Collaboration
+:copyright: 1998-2014, ALICE Experiment at CERN, All rights reserved
+
+:author: Markus Fasel , 
+:contact: <markus.fasel@cern.ch>
+:organization: Lawrence Berkeley National Laboratory
 """
 from base.Graphics import Frame
 
@@ -32,11 +32,29 @@ class NormalizedPtSpectrumFrame(Frame):
     def __init__(self, name):
         """
         Constructor
-        @param name: Name of the frame
+
+        :param name: Name of the frame
+        :type name: str
         """
         Frame.__init__(self, name, 0., 100., 1e-12, 10)
         self.SetXtitle("p_{t} (GeV/c)")
         self.SetYtitle("1/(2 #pi p_{t} N_{ev}) dN/(dp_{t} d#eta) (GeV/c)^{-2}")
+        
+class RawSpectrumFrame(Frame):
+    """
+    Frame class for event and bin-width normalized raw spectrum plots
+    """
+    
+    def __init__(self, name):
+        """ 
+        Constructor
+
+        :param name: Name of the frame
+        :type name: str
+        """
+        Frame.__init__(self, name, 0., 100., 1e-12, 100.)
+        self.SetXtitle("p_{t} (GeV/c)")
+        self.SetYtitle("1/N_{ev} dN/dp_{t} ((GeV/c)^{-1})")
         
 class EfficiencyFrame(Frame):
     """
@@ -46,7 +64,9 @@ class EfficiencyFrame(Frame):
     def __init__(self, name):
         """
         Constructor
-        @param name: Name of the frame
+
+        :param name: Name of the frame
+        :type name: str
         """
         Frame.__init__(self, name, 0., 100., 0., 1.)
         self.SetXtitle("p_{t, gen} (GeV/c)")
@@ -60,9 +80,13 @@ class SpectrumRatioFrame(Frame):
     def __init__(self, name, yrange, ytitle):
         """
         Constructor
-        @param name: Name of the frame
-        @param yrange: Dictonary of the plot range in y direction (keys min and max required)
-        @param ytitle: Title of the y-axis  
+
+        :param name: Name of the frame
+        :type name: str
+        :param yrange: Plot range in y direction (keys min and max required)
+        :type yrange: dictionary
+        :param ytitle: Title of the y-axis  
+        :type ytitle: str
         """
         Frame.__init__(self, name, 0., 100., yrange["min"], yrange["max"])
         self.SetXtitle("p_{t} (GeV/c)")
